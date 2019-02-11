3d8efd7f6973e61077faa283ef00ccdab59d137a
Tiago Marangoni <projetoarduino@gmail.com>
2019-02-11 14:36:42 -0200

    Initial release

---
+++ examples/main.go
@@ -0,0 +1,28 @@
+package main
+
+
+import (
+	"fmt"
+	"goled"
+)
+
+var oled = goled.NewDisplayBuffer(128,64)
+
+func main(){
+	fmt.Println("Oled Driver")
+	
+	oled.Init()
+
+	oled.WriteString("projetoarduino.com.br", 0 ,2, 1)
+
+
+	//oled.Draw_line(0, 0, 120, 0) 		//Barra horizontal superior
+	//oled.Draw_line(120, 0, 120, 60) 	//Barra Direita vertical
+	//oled.Draw_line(120, 59, 0, 59) 	//Barra inferior horizontal
+	//oled.Draw_line(0, 59, 0, 0); 	 	// Barra esquerda vertical
+
+	// oled.GenIcon(goled.Icon.Cloud, 60, 0)
+
+
+	oled.Display() //Chame essa função toda vez que quiser chamar algo novo
+}
